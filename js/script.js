500db750a028797c91c6c64001d8642140ac456b
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2022-02-26 21:03:37 +0600

    deleted extra script file

--- js/script.js
+++ js/script.js
@@ -45,3 +45,45 @@ vassabi.showTotalServed();
 vassabi.setServed(15);
 
 document.write("<hr>");
+
+
+
+// Info about Cars
+
+class Cars {
+    constructor(model, year, power, color) {
+      this.model = model;
+      this.year = year;
+      this.power = power + "hp";
+      this.color = color;
+    }
+
+  
+    showInfo() {
+        document.write(`Model is: ${this.model}<br> Year of issue is: ${this.year}<br> Power is: ${this.power}<br> Color is: ${this.color}<br>`)
+    }
+}
+
+class ElectroCars extends Cars {
+    constructor(model, year, power, color, battery) {
+      super(model, year, power, color);
+      this.battery = battery;
+    }
+
+    showInfo() {
+        super.showInfo();
+        document.write(`Battery range up to: ${this.battery} kilometers <br>`);
+    }
+
+    promote() {
+        document.write(`Buy ${this.model} and safe the planet!`)
+    }
+
+  }
+  
+const bmw = new Cars('BMW X6', 2019, 617, 'Black <br> <hr>');
+bmw.showInfo();
+
+const tesla = new ElectroCars("Tesla Model 3", 2022, 283, 'Silver', 593);
+tesla.showInfo();
+tesla.promote();
